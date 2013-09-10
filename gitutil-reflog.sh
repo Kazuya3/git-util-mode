ff5b85494c311234cae4c8e6cca7df9a0171f559
Kazuya <spgc2zr9@image.ocn.ne.jp>
2013-09-10 13:03:03 +0900

    情報行の区切りが空白だと、reflog subject が途中で 切れてしまうため、縦棒で区切ることにした。

--- gitutil-reflog.sh
+++ gitutil-reflog.sh
@@ -8,7 +8,7 @@ pwd >> ${GITLOG_FILE}
 
 # git log の実行結果を GITLOG_FILE に追記。
 # git log -g >> ${GITLOG_FILE}
-git log -20 -g --pretty=format:"%at %H %gN %gE %gD %gs%n%B" >> ${GITLOG_FILE}
+git log -20 -g --pretty=format:"%at|%H|%gN|%gE|%gD|%gs%n%B" >> ${GITLOG_FILE}
 
 # gitlog-mode を呼ぶ。
 D:/myRoot/opt/xyzzy/xyzzy.exe -l D:/myRoot/opt/xyzzy/site-lisp/gitlog-mode.l ${GITLOG_FILE}
