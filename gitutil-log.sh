9fdbf247e6c65ce074b4bb671d548192069a9d03
Kazuya <spgc2zr9@image.ocn.ne.jp>
2013-09-18 14:13:27 +0900

    「 gitutil-log.sh 」を引数省略で動作すするように変更した。 「 git log 」のファイル指定あり・無しと同じ感覚で使える。

--- gitutil-log.sh
+++ gitutil-log.sh
@@ -1,23 +1,32 @@
 GITLOG_FILE=D:/myRoot/home/.git/log.gitlog
 
-# 引数のファイルが存在するかチェック。
-if [ -e "$1" ]; then
-	
-	# モード識別子挿入。
-	echo "log" > ${GITLOG_FILE}
+# git から情報をファイルに出力し、gitlog-mode を起動するスクリプト。
+# 引数で任意のファイルを指定することができる。
 
-	# カレントディレクトリを GITLOG_FILE に追記。
-	pwd >> ${GITLOG_FILE}
-	
-	# 引数（対象ファイル）を GITLOG_FILE に追記。
-	echo "$1" >> ${GITLOG_FILE}
-	
-	# git log の実行結果を GITLOG_FILE に追記。
-#	git log "$1" >> ${GITLOG_FILE}
-	git log --name-status --pretty=format:"%at|%H|%aN|%aE|%n%B\`" >> ${GITLOG_FILE}
-	
-	# gitlog-mode を呼ぶ。
-	D:/myRoot/opt/xyzzy/xyzzy.exe -l D:/myRoot/opt/xyzzy/site-lisp/gitlog-mode.l ${GITLOG_FILE}
-else
-	echo -e "Error!\nSpecify file what you want to see log."
+path=""
+
+# ファイルの指定がある（引数がある）なら、
+# そのファイルが存在するかチェックする。
+if [ $# -eq 1 ]; then
+	if [ -e "$1" ]; then
+		path="$1"
+	else
+		echo -e "Error!\nReceived file does NOT exist."
+		exit 1
+	fi
 fi
+	
+# モード識別子挿入。
+echo "log" > ${GITLOG_FILE}
+
+# カレントディレクトリを GITLOG_FILE に追記。
+pwd >> ${GITLOG_FILE}
+
+# 引数（対象ファイル）を GITLOG_FILE に追記。
+echo "$1" >> ${GITLOG_FILE}
+
+# git log の実行結果を GITLOG_FILE に追記。
+git log --name-status --pretty=format:"%at|%H|%aN|%aE|%n%B\`" $path >> ${GITLOG_FILE}
+
+# gitlog-mode を呼ぶ。
+D:/myRoot/opt/xyzzy/xyzzy.exe -l D:/myRoot/opt/xyzzy/site-lisp/gitlog-mode.l ${GITLOG_FILE}
